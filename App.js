d3e9200eed3b11fcb71ed3211b82857bb4cc5a7b
Saumya Shekhar <saumya.shekhar@borderfree.io>
2023-08-04 21:06:16 +0530

    Project

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./components/WelcomeScreen/RootNavigation";
+import { createTable } from "./database";
 
 export default function App() {
+  createTable("master_data_user2");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+    // <StatusBar style="auto" />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
   },
 });
